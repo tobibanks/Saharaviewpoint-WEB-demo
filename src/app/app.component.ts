afcb794bc521a2c0fbf2699f11af5e15a0d6ef25
Mordecai Godwin <mogodwin@axamansard.com>
2023-12-09 21:21:31 +0100

    Initial set up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ResponsiveHelperComponent } from './shared/components/responsive-helper/responsive-helper.component';
+import { ThemeService } from './core/services/theme.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ResponsiveHelperComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'saharaviewpoint.WEB';
+  title = 'Saharaviewpoint WEB';
+
+  constructor(public themeService: ThemeService) {}
 }
---
+++ src/app/shared/directives/click-outside.directive.spec.ts
@@ -0,0 +1,9 @@
+import { ClickOutsideDirective } from "./click-outside.directive";
+
+
+// describe('ClickOutsideDirective', () => {
+//   it('should create an instance', () => {
+//     const directive = new ClickOutsideDirective();
+//     expect(directive).toBeTruthy();
+//   });
+// });
