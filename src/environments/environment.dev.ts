e3733249d1f6cbd095085f87b1f08906d8ebc796
Mordecai Godwin <mogodwin@axamansard.com>
2024-02-17 16:55:52 +0100

    used staging api

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,5 +2,6 @@ export const environment = {
   staging: false,
   production: false,
   env_name: 'Development',
-  apiUrl: 'https://localhost:7023/api/v1'
+  // apiUrl: 'https://localhost:7023/api/v1'
+  apiUrl: 'https://saharaviewpoint-api.azurewebsites.net/api/v1'
 };
