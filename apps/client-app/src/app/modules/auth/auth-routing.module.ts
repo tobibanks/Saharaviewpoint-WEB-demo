4d86792257123f85253cc14c84e3050dc7cc9e3e
Mordecai Godwin <mogodwin@axamansard.com>
2024-03-09 12:25:55 +0100

    abstracted auth pages in client app

--- apps/client-app/src/app/modules/auth/auth-routing.module.ts
+++ apps/client-app/src/app/modules/auth/auth-routing.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth.component';
-import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
-import { NewPasswordComponent } from './pages/new-password/new-password.component';
-import { SignInComponent } from './pages/sign-in/sign-in.component';
-import { SignUpComponent } from './pages/sign-up/sign-up.component';
-import { TwoStepsComponent } from './pages/two-steps/two-steps.component';
+import { SignInComponent, SignUpComponent, ForgotPasswordComponent, NewPasswordComponent, TwoStepsComponent } from '@svp-auth-components';
 
 const routes: Routes = [
   {
